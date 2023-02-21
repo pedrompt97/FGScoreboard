e12272f10ef6e939e539cd97cd4e463605579425
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2023-02-21 14:25:22 +0000

    Added players scene

---
+++ Overlay/objects/text2.js
@@ -0,0 +1,39 @@
+var text2 = {
+	pVal : $('#mText2'),
+	pText : true,
+	pDisappear : {
+		top : {
+			start : 901,
+			stop: 901,
+			units: 'px',
+			time: 0,
+			duration: 0.5,
+			effect:'easeIn'
+		},
+		opacity: {
+				start: 100,
+				stop: 0,
+				time: 0,
+				duration: 0.5,
+				effect: 'easeIn'
+		}
+	},
+	
+	pAppear : {
+		top: {
+			start: 901,
+			stop: 901,
+			units: 'px',
+			time: 0.5,
+			duration: 0.5,
+			effect:'easeOut'
+		},
+		opacity: {
+			start: 0,
+			stop: 100,
+			time: 0.5,
+			duration: 0.5,
+			effect: 'easeOut'
+		}
+	}
+};
---
+++ Overlay/objects/text4.js
@@ -0,0 +1,39 @@
+var text4 = {
+	pVal : $('#mText4'),
+	pText : true,
+	pDisappear : {
+		top : {
+			start : 970,
+			stop: 970,
+			units: 'px',
+			time: 0,
+			duration: 0.5,
+			effect:'easeIn'
+		},
+		opacity: {
+				start: 100,
+				stop: 0,
+				time: 0,
+				duration: 0.5,
+				effect: 'easeIn'
+		}
+	},
+	
+	pAppear : {
+		top: {
+			start: 970,
+			stop: 970,
+			units: 'px',
+			time: 0.5,
+			duration: 0.5,
+			effect:'easeOut'
+		},
+		opacity: {
+			start: 0,
+			stop: 100,
+			time: 0.5,
+			duration: 0.5,
+			effect: 'easeOut'
+		}
+	}
+};

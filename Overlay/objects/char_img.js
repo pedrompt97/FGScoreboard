7e799231defc4619ef3b459ddd30a873f6bf78ad
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2023-03-04 19:30:01 +0000

    Added support for more players in team games. Fixed a bunch of animaitons

---
+++ Overlay/objects/char_img.js
@@ -0,0 +1,37 @@
+var char_img = {
+	pDisappear : {
+		top: {
+			start : -6,
+			stop: -6,
+			units: 'px',
+			time: 0,
+			duration: 0.5,
+			effect:'easeIn'
+		},
+		opacity: {
+			start: 100,
+			stop: 0,
+			time: 0,
+			duration: 0.5,
+			effect: 'easeIn'
+		}
+	},
+	
+	pAppear : {
+		top: {
+			start: -6,
+			stop: -6,
+			units: 'px',
+			time: 0.5,
+			duration: 0.5,
+			effect:'easeOut'
+		},
+		opacity: {
+			start: 0,
+			stop: 100,
+			time: 0.5,
+			duration: 0.5,
+			effect: 'easeOut'
+		}
+	}
+};
